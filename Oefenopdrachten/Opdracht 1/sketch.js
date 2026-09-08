363a9d4f05dda6de54f152edfb495beb73983626
Calum123 <117999412+demonkid1@users.noreply.github.com>
2026-09-08 12:26:09 +0200

    oefen opdracht klaar en bezig met week opdracht

--- Oefenopdrachten/Opdracht 1/sketch.js
+++ Oefenopdrachten/Opdracht 1/sketch.js
@@ -1,7 +1,36 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(606, 406);
 }
 
 function draw() {
-  background(220);
+  background(255);
+  strokeWeight(4);
+  fill("nofill");
+  rect(4,4,600,400);
+  rect(4,4,450,50);
+  fill("blue");
+  rect(4,4,200,50);
+  fill("nofill");
+  rect(200,4,50,50);
+  rect(250,4,50,50);
+  fill("red");
+  rect(450,4,154,50);
+  fill("yellow");
+  rect(4,54,100,100);
+  fill("black");
+  rect(104,54,96,70);
+  fill("nofill");
+  rect(4,154,100,200);
+  fill("blue");
+  rect(4,354,300,50);
+  fill("red");
+  rect(104,200,100,100);
+  fill("yellow");
+  rect(204,54,100,300);
+  fill("red");
+  rect(304,54,100,150);
+  fill("yellow");
+  rect(404,54,200,170);
+  fill("red")
+  rect(304,224,300,100);
 }
--- Week 1/sketch.js
+++ Week 1/sketch.js
@@ -4,4 +4,14 @@ function setup() {
 
 function draw() {
   background(220);
+  fill("grey")
+  rect (45,50,50,150);
+  fill("red")
+  circle(70,80,40);
+  fill("orange")
+  circle(70,125,40);
+  fill("green")
+  circle(70,170,40);
+  fill("grey")
+  rect (60,200,20,120);
 }

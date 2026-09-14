3529394c813c542b9a625dccd430cca6676e0cae
Calum123 <117999412+demonkid1@users.noreply.github.com>
2026-09-14 11:40:49 +0200

    push

--- Oefenopdrachten/Opdracht 2/sketch.js
+++ Oefenopdrachten/Opdracht 2/sketch.js
@@ -4,4 +4,27 @@ function setup() {
 
 function draw() {
   background(220);
+
+  let x = 100
+  let y = 80
+  let greeting = "Hello World!"
+  let a = 20
+  let b = 10
+  let optellen = a + b
+  let aftrekken = a - b
+  let vermenigvuldigen = a * b
+  let delen = a / b
+
+  text(x, 20, 20);
+  text(greeting, 20, 60);
+
+  text("Optellen:  " + optellen, 20, y);
+  noLoop(y = y + 20);
+  text("Aftrekken:  " + aftrekken, 20, y);
+  noLoop(y = y + 20);
+  text("Vermenigvuldigen:  " + vermenigvuldigen, 20, y);
+  noLoop(y = y + 20);
+  text("Delen:  " + delen, 20, y);
+  //Ik heb het gemaakt zodat het elke keer noloop doet want anders rent het code weg.
+  //ik heb de berekeningen met +, -, * en / gedaan en met variable gewerkt.
 }

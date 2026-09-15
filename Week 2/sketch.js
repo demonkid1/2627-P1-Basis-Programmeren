f0685dfd19da07867e34622bf1da639b55ae19e3
Calum123 <117999412+demonkid1@users.noreply.github.com>
2026-09-15 13:40:25 +0200

    save2

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -5,38 +5,48 @@ function setup() {
 function draw() {
   background("lightblue");
   //variablelen
-  let Y = 1
+  let Y = 10
   let ZonSp = 50 + Y
+  let MV = Y + ZonSp
   //zon
-  circle (50, ZonSp, 50)
+  fill('yellow')
+  circle(MV, 50, 50)
   //bergen
-  strokeWeight (0)
+  strokeWeight(0)
   fill("grey")
-  triangle (0,500, 250, 100, 500, 500)
-  triangle (450,500, 750, 125, 1000, 500)
-//auto weg
-  fill ("black");
-  rect (0,500,800,100);
+  triangle(0, 500, 250, 100, 500, 500)
+  triangle(450, 500, 750, 125, 1000, 500)
   //grass
   fill("green")
-  rect(0,480, 800,20)
+  rect(0, 480, 800, 20)
   fill("darkgreen")
-  rect(0,480, 800,10)
-//gele lijnen voor de weg
-  fill ("yellow")
-  rect (0, 538, 50, 25)
-  rect (100, 538, 50, 25)
-  rect (200, 538, 50, 25)
-  rect (300, 538, 50, 25)
-  rect (400, 538, 50, 25)
-  rect (500, 538, 50, 25)
-  rect (600, 538, 50, 25)
-  rect (700, 538, 50, 25)
-  rect (800, 538, 50, 25)
+  rect(0, 480, 800, 10)
+  //auto weg
+  fill("black");
+  rect(0, 500, 800, 100);
+  //gele lijnen voor de weg
+  fill("yellow")
+  rect(0, 538, 50, 25)
+  rect(100, 538, 50, 25)
+  rect(200, 538, 50, 25)
+  rect(300, 538, 50, 25)
+  rect(400, 538, 50, 25)
+  rect(500, 538, 50, 25)
+  rect(600, 538, 50, 25)
+  rect(700, 538, 50, 25)
+  rect(800, 538, 50, 25)
+  //auto rood
+  fill("grey")
+  circle(100, 530, 20)
+  circle(150, 530, 20)
+  fill("red")
+  rect(90, 500, 90, 25)
+  rect(90, 485, 70, 25)
 
 
 
 
 
-  
+
+
 }

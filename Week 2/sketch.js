8c7a70eb80276fc569d231d373e660c8e9f5787a
Calum123 <117999412+demonkid1@users.noreply.github.com>
2026-09-15 11:59:34 +0200

    save 1

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -4,9 +4,34 @@ function setup() {
 
 function draw() {
   background("lightblue");
+  //bergen
+  strokeWeight (0)
+  fill("grey")
+  triangle (0,500, 250, 20, 500, 500)
+  triangle (450,500, 750, 20, 1000, 500)
+//auto weg
+  fill ("black");
+  rect (0,500,800,100);
+  //grass
+  fill("green")
+  rect(0,480, 800,20)
+  fill("darkgreen")
+  rect(0,480, 800,10)
+//gele lijnen voor de weg
+  fill ("yellow")
+  rect (0, 538, 50, 25)
+  rect (100, 538, 50, 25)
+  rect (200, 538, 50, 25)
+  rect (300, 538, 50, 25)
+  rect (400, 538, 50, 25)
+  rect (500, 538, 50, 25)
+  rect (600, 538, 50, 25)
+  rect (700, 538, 50, 25)
+  rect (800, 538, 50, 25)
 
-  fill ("lightgrey");
-  rect (0,550,800,50);
 
-  fill("yellow");
+
+
+
+  
 }

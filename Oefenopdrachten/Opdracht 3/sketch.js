6e9eae09beb6f46f87d509c8e63320fef2eab8f3
Calum123 <117999412+demonkid1@users.noreply.github.com>
2026-09-15 11:17:52 +0200

    week opdracht

--- Oefenopdrachten/Opdracht 3/sketch.js
+++ Oefenopdrachten/Opdracht 3/sketch.js
@@ -1,7 +1,31 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 200);
+  
 }
 
 function draw() {
   background(220);
+  let score = random(100)
+  noLoop(score)
+  if (score >= 90) {
+    fill ("green")
+    text ("Uitstekend!", 20,20)
+  } else if (score => 70 && score <= 89) {
+    fill ("yellow")
+    text ("Goed gedaan!", 20,20)
+
+  } else if (score => 50 && score <= 69) {
+    fill ("orange")
+    text ("Voldoende.", 20,20)
+
+  } else if (score => 0 && score <= 49){
+    fill ("red")
+    text ("Onvoldoende", 20,20)
+  }
+  
+  
+
+  
+
+
 }

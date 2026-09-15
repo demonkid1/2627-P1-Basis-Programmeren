7c2f4fde0d2cee5bd00c9d2b5abbed2431b0c7bd
Calum123 <117999412+demonkid1@users.noreply.github.com>
2026-09-15 14:58:36 +0200

    save3

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -5,25 +5,28 @@ function setup() {
 function draw() {
   background("lightblue");
   //variablelen
-  let Y = 10
-  let ZonSp = 50 + Y
-  let MV = Y + ZonSp
+
+
   //zon
   fill('yellow')
-  circle(MV, 50, 50)
+  circle(100, 100 + 50, 50)
+
   //bergen
   strokeWeight(0)
   fill("grey")
   triangle(0, 500, 250, 100, 500, 500)
   triangle(450, 500, 750, 125, 1000, 500)
+
   //grass
   fill("green")
   rect(0, 480, 800, 20)
   fill("darkgreen")
   rect(0, 480, 800, 10)
+
   //auto weg
   fill("black");
   rect(0, 500, 800, 100);
+
   //gele lijnen voor de weg
   fill("yellow")
   rect(0, 538, 50, 25)
@@ -35,6 +38,7 @@ function draw() {
   rect(600, 538, 50, 25)
   rect(700, 538, 50, 25)
   rect(800, 538, 50, 25)
+
   //auto rood
   fill("grey")
   circle(100, 530, 20)
@@ -42,11 +46,18 @@ function draw() {
   fill("red")
   rect(90, 500, 90, 25)
   rect(90, 485, 70, 25)
-
-
-
-
-
-
-
+  //auto licht groen
+  fill("grey")
+  circle(100, 530, 20)
+  circle(150, 530, 20)
+  fill("lightgreen")
+  rect(90, 500, 90, 25)
+  rect(90, 485, 70, 25)
+  //auto blauw
+  fill("grey")
+  circle(100, 530, 20)
+  circle(150, 530, 20)
+  fill("blue")
+  rect(90, 500, 90, 25)
+  rect(90, 485, 70, 25)
 }

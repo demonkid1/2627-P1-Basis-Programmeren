3efa29766ebaff71d7d7a28185111fb72260ab3c
Calum123 <117999412+demonkid1@users.noreply.github.com>
2026-09-21 11:55:50 +0200

    start

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -1,7 +1,22 @@
+let Player = 1
+let Box1 = 0
+let Box2 = 0
+let Box3 = 0
+let Box4 = 0
+let Box5 = 0
+let Box6 = 0
+let Box7 = 0
+let Box8 = 0
+let Box9 = 0
+
+
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
 }
 
 function draw() {
   background(220);
+
 }
